03e928c06da84dccefb601f6bd2255897171d034
Matthew Steffen <msteffen@pachyderm.io>
2017-11-15 10:13:29 -0800

    Add prints to start-kube-docker to try and figure out why travis is stalling

--- etc/kube/start-kube-docker.sh
+++ etc/kube/start-kube-docker.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 set -Ee
+
+# Prints a spinning wheel. Every time you call it, the wheel advances 1/4 turn
+WHEEL="-\|/"
+spin() {
+    echo -en "\e[D${WHEEL:0:1}"
+    WHEEL=${WHEEL:1}${WHEEL:0:1}
+}
 
 docker run \
     -d \
@@ -15,4 +22,6 @@ docker run \
     --privileged=true \
     gcr.io/google_containers/hyperkube:v1.7.10 \
     sh "/rootfs/$PWD/etc/kube/internal.sh"
-until kubectl version 2>/dev/null >/dev/null; do sleep 5; done
+echo waiting for kubectl version to pass...
+until kubectl version 2>/dev/null >/dev/null; do spin; sleep 5; done
+echo kubectl version passed
